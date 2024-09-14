1cb6e03e8d9686c6e0d85b04187b5dffe95a49dd
ahmad <ahm4d.bilal@gmail.com>
2024-09-14 21:12:49 +0500

    Solved 20

--- ValidParentheses/Solution.java
+++ ValidParentheses/Solution.java
@@ -1,24 +1,46 @@
 package ValidParentheses;
+
 import java.util.Stack;
 
-public class Solution {
+class Solution {
+    // Method to check if a string of brackets is valid
     public boolean isValid(String s) {
+        // Initialize a stack to keep track of opening brackets
         Stack<Character> bracketStack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            char currentChar = s.charAt(i);
-            boolean isOpeningBracket = currentChar == '{' || currentChar == '(' || currentChar == '[';
-            char lastChar = ' ';
-            if(!bracketStack.isEmpty()) lastChar = bracketStack.lastElement();
-            boolean isCorrectClosingBracket = (currentChar == '}' && lastChar == '{') || (currentChar == ')' && lastChar == '(') || (currentChar == ']' && lastChar == '[');
 
+        // Iterate over each character in the string
+        for (int i = 0; i < s.length(); i++) {
+            // Get the current character from the string
+            char currentChar = s.charAt(i);
+
+            // Check if the current character is an opening bracket
+            boolean isOpeningBracket = currentChar == '{' || currentChar == '(' || currentChar == '[';
+
+            // Initialize a variable to hold the last character in the stack, set to space initially
+            char lastChar = ' ';
+            // If the stack is not empty, get the top element (last inserted opening bracket)
+            if (!bracketStack.isEmpty()) lastChar = bracketStack.lastElement();
+
+            // Check if the current character is a correct closing bracket for the top element of the stack
+            boolean isCorrectClosingBracket =
+                    (currentChar == '}' && lastChar == '{') ||
+                            (currentChar == ')' && lastChar == '(') ||
+                            (currentChar == ']' && lastChar == '[');
+
+            // If it's an opening bracket, push it onto the stack
             if (isOpeningBracket) {
                 bracketStack.push(currentChar);
-            } else if (isCorrectClosingBracket){
+            }
+            // If it's a correct closing bracket, pop the last opening bracket from the stack
+            else if (isCorrectClosingBracket) {
                 bracketStack.pop();
             }
-
-            System.out.println(bracketStack);
+            // If neither, return false as the string is invalid
+            else {
+                return false;
+            }
         }
+        // If the stack is empty at the end, all brackets were matched, so return true
         return bracketStack.isEmpty();
     }
 }
